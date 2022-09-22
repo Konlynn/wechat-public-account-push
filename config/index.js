4a814a6cc9f82ea02046c149243d67f4d85886f2
Konlynn <52777366+Konlynn@users.noreply.github.com>
2022-09-23 01:02:33 +0800

    Update index.js

--- config/index.js
+++ config/index.js
@@ -64,7 +64,7 @@ export const config = {
       // 扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: "oW89N5tEDS_DukmLQCXzCpaQiMpk",
       // 你想对他发送的模板消息的模板ID
-      useTemplateId: "WKW2qkIeaWPVAjYzYlkYqJSERPr3xRIhoFw-NVBwQrg",
+      useTemplateId: ["WKW2qkIeaWPVAjYzYlkYqJSERPr3xRIhoFw-NVBwQrg","oW89N5tJetZfnvfMWdyyYScuGAdI"],
       // 所在省份
       province: "广东",
       // 所在城市
